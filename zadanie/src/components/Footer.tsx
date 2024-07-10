b2c0a7bbace3bae15266ee3ac0945637b277568d
krystiano13 <crystianotv@gmail.com>
2024-07-10 14:11:01 +0200

    create 'CSS IS AWESOME' button styles

---
+++ zadanie/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__box_wrapper">
+        <div className="footer__box_wrapper__box"></div>
+        <div className="footer__box_wrapper__text">
+          <p className="footer__box_wrapper__text__p">CSS</p>
+          <p className="footer__box_wrapper__text__p">IS</p>
+          <p className="footer__box_wrapper__text__p">AWESOME</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
