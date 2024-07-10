3b96d2358806d1d2e269d5c0805397d630f9fecc
krystiano13 <crystianotv@gmail.com>
2024-07-10 12:33:04 +0200

    configure react-router

--- zadanie/src/App.tsx
+++ zadanie/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
+
 function App() {
-  return <>Siema</>;
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
